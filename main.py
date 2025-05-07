799432838478a7b5eb65a1ebb017202058f2932f
cesarleyton <cesarleyton549@gmail.com>
2025-05-06 22:15:40 -0500

    draw the player on the screen and add basic movements

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import pygame
 from constants import *
+from player import Player
+
+
+
+
 
 def main():
 
@@ -10,17 +15,25 @@ def main():
     # and returns a tuple with the number of modules initialized and the list of initialized modules 
     initGame = pygame.init()
 
+    clock = pygame.time.Clock()
+
+    dt = 0
     # Creates a display using, recieves a tuple as argument indicating the width and height of the window
     # The display is a surface that will be used to draw the game
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         screen.fill((0, 0, 0)) 
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
+            
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
