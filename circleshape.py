5617d25d3d5050f0bb9100edc473e5809805cb71
cesarleyton <cesarleyton549@gmail.com>
2025-05-07 06:35:15 -0500

    move the player instance to a pygame group

--- circleshape.py
+++ circleshape.py
@@ -8,9 +8,11 @@ class CircleShape(pygame.sprite.Sprite):
             super().__init__()
         
         self.position = pygame.Vector2(x,y)
+
+        #Velocity is 0 at first, the player isn't moving in any direction and has no speed.
         self.velocity = pygame.Vector2(0,0)
         self.radius = radius
-        
+
         def draw(self, screen):
 
         # sub-classes must override
--- main.py
+++ main.py
@@ -9,25 +9,59 @@ from player import Player
 def main():
 
     print("Starting Asteroids!")
-    print("Screen width:", SCREEN_WIDTH)
-    print("Screen height:", SCREEN_HEIGHT)
+
     # Initialize the pygame library, it automatically initializes all the modules
     # and returns a tuple with the number of modules initialized and the list of initialized modules 
     initGame = pygame.init()
 
     clock = pygame.time.Clock()
 
+    updatable = pygame.sprite.Group()
+
+    drawable = pygame.sprite.Group()
+
+
+    Player.containers = (updatable, drawable)
+
     dt = 0
     # Creates a display using, recieves a tuple as argument indicating the width and height of the window
     # The display is a surface that will be used to draw the game
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    #Create an instance of the player 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    #Start game loop, it must check player input, update the world(process physics and animations)
+    #draw the updated world to the screen
     while True:
+
+
+        # Fill the screen with a color, black in this case
         screen.fill((0, 0, 0)) 
-        player.draw(screen)
-        player.update(dt)
+
+        #We can manually call each method of an object, but that can make the code very cluttered
+        #For example, we could have many players and have 10 lines calling the update, pygame
+        # offers the group class that can manage multiple groups, think of a group as a venn diagram
+        # if an object belongs to a group it means it has a specific method that can be called
+        # e.g If player belongs to the drawable group, it can call the draw() method.
+
+        # player.draw(screen)
+        # player.update(dt)
+
+        # It's possible to call a method of an object in a group by iterating each item of the group
+        # or calling the method directly on the group which calls it on every item at the same time.
+
+        #Draw the player in the screen
+        updatable.update(dt)
+
+        for member in drawable:
+            member.draw(screen)
+
+     
+
+        #Update the screen
         pygame.display.flip()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
